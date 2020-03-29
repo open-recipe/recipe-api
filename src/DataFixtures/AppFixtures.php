2563f50af75e038f58954ecb3f89ed70697dd5d5
Alban Guerbette <aguerbette@gmail.com>
2020-03-29 23:09:38 +0200

    Add user fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Comprendre;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // create 20 users! Bam!
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User(sprintf('user%d@example.com', $i));
+            $user->setPassword('123456');
+            $manager->persist($user);
+        }
+
+        $manager->flush();  
+    }
+}

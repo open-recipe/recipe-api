a7adc82f115787f16fb5ec33b9e6fa8418529881
Alban Guerbette <aguerbette@gmail.com>
2020-03-29 20:46:58 +0200

    Fix ecs

--- src/Migrations/Version20200329161012.php
+++ src/Migrations/Version20200329161012.php
@@ -12,12 +12,12 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20200329161012 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
@@ -30,7 +30,7 @@ final class Version20200329161012 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_DA88B137A76ED395 ON recipe (user_id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');

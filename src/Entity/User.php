34def7f723193c04fa7c0a0485e636ce5797c777
Alban Guerbette <aguerbette@gmail.com>
2020-03-29 15:28:11 +0200

    Ecs Fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -17,14 +19,10 @@ class User implements UserInterface
      */
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=180, unique=true)
-     */
+    /** @ORM\Column(type="string", length=180, unique=true) */
     private $email;
 
-    /**
-     * @ORM\Column(type="json")
-     */
+    /** @ORM\Column(type="json") */
     private $roles = [];
 
     /**
